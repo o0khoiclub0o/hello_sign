fb748aa980a5a15eea7f00aef8d3764d182bd831
Võ Anh Duy <voanhduy1512@live.com>
2014-02-18 00:06:49 +0700

    add coverall

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,10 @@
 # Require this file using `require "spec_helper"` to ensure that it is only
 # loaded once.
 #
+if Rails.env.travis?
+  require 'coveralls'
+  Coveralls.wear!
+end
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
